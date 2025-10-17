3a213da10e969881295c3d267d52f1ba64e185c4
Samuel Alves Doria <samueldoria500@gmail.com>
2025-10-17 14:43:13 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,12 @@ function toggleMenu() {
   const menu = document.querySelector(".menu");
   menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
+function toggleHighContrast() {
+  document.body.classList.toggle('high-contrast');
+}
+
